97c44256d57fe393ea6ba1bd5eb1c063de98ac0e
aashish-nomad <aashish.nomad@gmail.com>
2025-07-01 15:34:37 +0545

    Computes the return value and fixes the type conversion issue from string to number

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import SiteHeader from "./components/SiteHeader";
 import UserInput from "./components/UserInput";
 import InputField from "./components/InputField";
 import { useState } from "react";
+import Results from "./components/Results";
 
 function App() {
 
   const [investmentFields, setInvestmentFields] = useState({
     initialInvestment: 15000,
     annualInvestment: 1500,
-    expectedReturns: 6,
+    expectedReturn: 6,
     duration: 10
   });
 
@@ -16,24 +17,25 @@ function App() {
     setInvestmentFields((prevValue) => {
       return {
         ...prevValue,
-        [identifier]: newValue
+        [identifier]: +newValue // Adding this plus will convert string type to integer type.
       }
     });
   }
 
   return (
     <>
-      <SiteHeader title="Investment Course" />
+      <SiteHeader title="Investment App" />
       <UserInput>
         <div className="input-group">
           <InputField id='initialInvestment' label='Initial Investment' value={investmentFields.initialInvestment} handleChange={handleChangeOnInvestmentFields} />
           <InputField id='annualInvestment' label='Annual Investment' value={investmentFields.annualInvestment} handleChange={handleChangeOnInvestmentFields} />
         </div>
         <div className="input-group">
-          <InputField id='expectedReturns' label='Expected Investment' value={investmentFields.expectedReturns} handleChange={handleChangeOnInvestmentFields} />
+          <InputField id='expectedReturn' label='Expected Investment' value={investmentFields.expectedReturn} handleChange={handleChangeOnInvestmentFields} />
           <InputField id='duration' label='Duration' value={investmentFields.duration} handleChange={handleChangeOnInvestmentFields} />
         </div>
       </UserInput>
+      <Results input={investmentFields} />
     </>
   )
 }
